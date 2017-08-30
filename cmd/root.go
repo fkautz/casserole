40c200e3050374b3f1c46390d4ba54d422dd7949
Frederick F. Kautz IV <fkautz@alumni.cmu.edu>
2017-08-29 19:07:59 -0700

    Updating the cli description

--- cmd/root.go
+++ cmd/root.go
@@ -28,16 +28,10 @@ var cfgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "casserole",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: RunMain,
+	Short: "Distributed HTTP Cache",
+	Long: `Casserole is a distributed HTTP cache that uses group cache to
+	share the load between servers. An optional disk cache that integrates
+	with groupcache is also provided.`,
 }
 
 // Execute adds all child commands to the root command sets flags appropriately.
